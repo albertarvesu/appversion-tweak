032af30b067a5441d5e85a7d303f99108e023431
Albert Arvesu <albertarvesu@gmail.com>
2014-01-07 14:22:55 +0800

    added necessary files

--- www/AppVersionPlugin.js
+++ www/AppVersionPlugin.js
@@ -1,11 +1,7 @@
-cordova.define("uk.co.whiteoctober.cordova.AppVersion.AppVersionPlugin", function(require, exports, module) {
+var exec = require('cordova/exec');
 
-	var exec = require('cordova/exec');
+var getAppVersion = function (success, fail) {
+	exec(success, fail, "AppVersion", "getVersionNumber", []);
+};
 
-	var getAppVersion = function (success, fail) {
-		exec(success, fail, "AppVersion", "getVersionNumber", []);
-	};
-
-	module.exports = getAppVersion;
-});
-
+module.exports = getAppVersion;
